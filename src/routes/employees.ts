a7ff40af924016f737714f6da4cb5fd5109eb3c3
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-10-29 19:21:55 +0100

    Get employees by date of creation

--- src/routes/employees.ts
+++ src/routes/employees.ts
@@ -3,13 +3,36 @@ import Employee from '../models/Employee';
 
 const router = Router();
 
-router.post('/employees', async (req, res) => {
+// Get all employees
+router.get('/employees', async (req, res) => {
   try {
-    const newEmployee = new Employee(req.body);
-    await newEmployee.save();
-    res.status(201).json(newEmployee);
+    const employees = await Employee.find({});
+    res.json(employees);
   } catch (error: any) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get employee by date
+router.get('/employees/by-date', async (req, res) => {
+  try {
+    const { date } = req.query;
+    if (!date) {
+      return res
+        .status(400)
+        .json({ error: "You must provide a 'date' query parameter" });
+    }
+
+    const employees = await Employee.find({
+      dateCreated: {
+        $gte: new Date(date as string),
+        $lt: new Date(new Date(date as string).getTime() + 24 * 60 * 60 * 1000),
+      },
+    });
+
+    res.json(employees);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
